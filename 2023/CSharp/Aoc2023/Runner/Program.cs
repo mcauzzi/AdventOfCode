07ee8dce6a06edde4955af2ff97019147e3d2e9f
marco <furai93@gmail.com>
2023-12-04 11:05:06 +0100

    Added Day 4

--- 2023/CSharp/Aoc2023/Runner/Program.cs
+++ 2023/CSharp/Aoc2023/Runner/Program.cs
@@ -1,7 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Diagnostics;
 using Implementations.Aoc3;
+using Implementations.Aoc4;
 
-var aoc=new Aoc3();
+var aoc =new Aoc4();
+var st  = new Stopwatch();
+st.Start();
 Console.WriteLine(aoc.SolvePart1());
+st.Stop();
+Console.WriteLine($"Time for part 1:{st.ElapsedMilliseconds}ms");
+st.Restart();
 Console.WriteLine(aoc.SolvePart2());
+st.Stop();
+Console.WriteLine($"Time for part 2:{st.ElapsedMilliseconds}ms");
