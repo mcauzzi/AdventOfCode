cac5335376eb80581e9e9c12cf6422c2946bbeac
Marco Cauzzi <furai93@gmail.com>
2023-12-07 15:08:25 +0100

    Added day 7

---
+++ 2023/CSharp/Aoc2023/Implementations/Aoc7/Aoc7.cs
@@ -0,0 +1,20 @@
+using Implementations.Helpers;
+
+namespace Implementations.Aoc7;
+
+public class Aoc7 : IAoc<int, int>
+{
+    private string[] Input { get; } = File.ReadAllLines("./Aoc7/Input.txt");
+
+    public int SolvePart1()
+    {
+        var game = new Game(Input);
+        return game.TotalPower;
+    }
+
+    public int SolvePart2()
+    {
+        var game = new GameWithJokers(Input);
+        return game.TotalPower;
+    }
+}
