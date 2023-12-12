cd2e060849f58b8b4c69ff93d520ad4afe5332f5
marco <furai93@gmail.com>
2023-12-12 09:41:55 +0100

    Added day 11 part 2

--- 2023/CSharp/Aoc2023/Implementations/Aoc11/Aoc11.cs
+++ 2023/CSharp/Aoc2023/Implementations/Aoc11/Aoc11.cs
@@ -7,55 +7,32 @@ public class Aoc11 : IAoc<double, double>
     private char[][] Input { get; set; } =
         File.ReadAllLines("./Aoc11/Input.txt").Select(x => x.Select(x => x).ToArray()).ToArray();
 
-    public Dictionary<(Galaxy, Galaxy), double> Distances { get; set; } = new();
-
     public double SolvePart1()
     {
-        for (int i = 0; i < Input.Length; i++)
-        {
-            if (Input[i].All(x => x == '.'))
-            {
-                var newArray = new char[Input[i].Length];
-                Array.Fill(newArray, '.');
-                Input = Input[..i].Concat(new[] { newArray }).Concat(Input[i..]).ToArray();
-                i++;
-            }
-        }
-
-        for (int i = 0; i < Input[0].Length; i++)
-        {
-            if (Input.Select(x => x[i]).All(x => x == '.'))
-            {
-                for (var j = 0; j < Input.Length; j++)
-                {
-                    Input[j] = Input[j][..i].Append('.').Concat(Input[j][i..]).ToArray();
-                }
-
-                i++;
-            }
-        }
+        Dictionary<(Galaxy, Galaxy), double> distances  = new();
 
         var galaxies = Input.SelectMany((x, row) => x.Select((x, col) => new { col, row, val = x })
                                                      .Where(x => x.val == '#').Select((y) => new Galaxy(y.col, y.row)))
                             .ToList();
-        CalculateGalaxiesDistances(galaxies);
+        ExpandGalaxies(galaxies, 1);
+        CalculateGalaxiesDistances(distances,galaxies);
         foreach (var charArray in Input)
         {
             Console.WriteLine(new string(charArray));
         }
 
-        return Distances.Sum(x => x.Value);
+        return distances.Sum(x => x.Value);
     }
 
-    private void CalculateGalaxiesDistances(List<Galaxy> galaxies)
+    private void CalculateGalaxiesDistances(Dictionary<(Galaxy, Galaxy), double> distances, List<Galaxy> galaxies)
     {
         foreach (var gal in galaxies)
         {
             foreach (var other in galaxies.Where(x => x != gal))
             {
-                if (!Distances.TryGetValue((gal, other), out _) && !Distances.TryGetValue((other, gal), out _))
+                if (!distances.TryGetValue((gal, other), out _) && !distances.TryGetValue((other, gal), out _))
                 {
-                    Distances.Add((gal, other), gal.DistanceBetweenGalaxies(other));
+                    distances.Add((gal, other), gal.DistanceBetweenGalaxies(other));
                 }
             }
         }
@@ -64,13 +41,29 @@ public class Aoc11 : IAoc<double, double>
 
     public double SolvePart2()
     {
+        Dictionary<(Galaxy, Galaxy), double> distances  = new();
+        var galaxies = Input.SelectMany((x, row) => x.Select((x, col) => new { col, row, val = x })
+                                                     .Where(x => x.val == '#').Select((y) => new Galaxy(y.col, y.row)))
+                            .ToList();
+        ExpandGalaxies(galaxies,999999);
+        CalculateGalaxiesDistances(distances,galaxies);
+        foreach (var galaxy in galaxies)
+        {
+            Console.WriteLine($"Id:{galaxy.Id}, RealCol:{galaxy.RealColumn}, RealRow:{galaxy.RealRow}");
+        }
+        return distances.Sum(x => x.Value);
+    }
+
+    private void ExpandGalaxies(List<Galaxy> galaxies,long expansion)
+    {
         for (int i = 0; i < Input.Length; i++)
         {
             if (Input[i].All(x => x == '.'))
             {
-                var newRows = Enumerable.Repeat(Enumerable.Repeat('.', Input[i].Length).ToArray(), 1000000).ToArray();
-                Input = Input[..i].Concat(newRows).Concat(Input[i..]).ToArray();
-                i++;
+                foreach (var galaxy in galaxies.Where(x=>x.Row>i))
+                {
+                    galaxy.RealRow += expansion;
+                }
             }
         }
 
@@ -78,21 +71,12 @@ public class Aoc11 : IAoc<double, double>
         {
             if (Input.Select(x => x[i]).All(x => x == '.'))
             {
-                for (var j = 0; j < Input.Length; j++)
+                foreach (var galaxy in galaxies.Where(x=>x.Column>i))
                 {
-                    Input[j] = Input[j][..i].Concat(Enumerable.Repeat('.', 1000000)).Concat(Input[j][i..]).ToArray();
+                    galaxy.RealColumn += expansion;
                 }
-
-                i++;
             }
         }
-
-        var galaxies = Input.SelectMany((x, row) => x.Select((x, col) => new { col, row, val = x })
-                                                     .Where(x => x.val == '#').Select((y) => new Galaxy(y.col, y.row)))
-                            .ToList();
-        CalculateGalaxiesDistances(galaxies);
-
-        return Distances.Sum(x => x.Value);
     }
 }
 
@@ -100,20 +84,24 @@ public class Galaxy : IEquatable<Galaxy>
 {
     public Galaxy(int column, int row)
     {
-        Column = column;
-        Row    = row;
-        Id     = GalaxyCounter++;
+        Column     = column;
+        Row        = row;
+        RealRow    = row;
+        RealColumn = column;
+        Id         = GalaxyCounter++;
     }
 
     public double DistanceBetweenGalaxies(Galaxy other)
     {
-        return Math.Abs(other.Column - Column) + Math.Abs(other.Row - Row);
+        return Math.Abs(other.RealColumn - RealColumn) + Math.Abs(other.RealRow - RealRow);
     }
 
-    public         int Column        { get; }
-    public         int Row           { get; }
-    public         int Id            { get; }
-    private static int GalaxyCounter { get; set; } = 1;
+    public         long Column        { get; }
+    public         long Row           { get; }
+    public         long RealRow       { get; set; }
+    public         long RealColumn    { get; set; }
+    public         int  Id            { get; }
+    private static int  GalaxyCounter { get; set; } = 1;
 
     public bool Equals(Galaxy? other)
     {
