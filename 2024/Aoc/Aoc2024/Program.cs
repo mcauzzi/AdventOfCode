d9b996293b808266aa4cf1be760a9092155ddf18
mcauzzi <m.cauzzi93@gmail.com>
2024-12-01 19:01:25 +0100

    Aggiunta soluzione 2024 Aggiunto primo giorno

---
+++ 2024/Aoc/Aoc2024/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Aoc2024._01;
+
+var sol=new Solution0124();
+Console.WriteLine(sol.Part1());
+Console.WriteLine(sol.Part2());
