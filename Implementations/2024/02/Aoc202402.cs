fae6917867f9af4976906e5a7b7eeef1fe603cbc
mcauzzi <m.cauzzi93@gmail.com>
2024-12-02 22:42:30 +0100

    Added second day solution

---
+++ Implementations/2024/02/Aoc202402.cs
@@ -0,0 +1,106 @@
+using Implementations.Helpers;
+
+namespace Implementations._2024._02;
+
+public class Aoc202402 : IAoc<long, long>
+{
+    public Aoc202402()
+    {
+        Reports = File.ReadAllLines("2024/02/Input.txt")
+                      .Select(x => x.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+                      .Select(x=>x.Select(int.Parse).ToList())
+                      .ToList();
+    }
+
+    public List<List<int>> Reports { get; set; }
+
+    public long SolvePart1()
+    {
+        var safeReports = 0;
+        foreach (var report in Reports)
+        {
+            if (IsReportSafe(report))
+            {
+                safeReports++;
+            }
+        }
+
+        return safeReports;
+    }
+
+    private bool IsReportSafe(List<int> report)
+    {
+        bool? isAsc = null;
+        for (int i = 0; i < report.Count; i++)
+        {
+            if (i == 0)
+            {
+                continue;
+            }
+
+            var curr = report[i];
+            var prev    = report[i-1];
+            var diff =Math.Abs(curr-prev);
+            if (diff < 1 || diff > 3)
+            {
+                return false;
+            }
+
+            switch (isAsc)
+            {
+                case null when curr > prev:
+                    isAsc=true;
+                    continue;
+                case null when curr<prev:
+                    isAsc=false;
+                    continue;
+            }
+
+            if(curr>prev && isAsc==false)
+            {
+                return false;
+            }
+            if(curr<prev && isAsc==true)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public long SolvePart2()
+    {
+        var safeReports = 0;
+        foreach (var report in Reports)
+        {
+            if (IsReportSafe(report))
+            {
+                safeReports++;
+            }
+            else
+            {
+                if (IsReportSafeWithOneLess(report))
+                {
+                    safeReports++;
+                }
+            }
+        }
+
+        return safeReports;
+    }
+
+    private bool IsReportSafeWithOneLess(List<int> report)
+    {
+        for (int i = 0; i < report.Count; i++)
+        {
+            var temp = new List<int>(report);
+            temp.RemoveAt(i);
+            if (IsReportSafe(temp))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
--- Runner/Program.cs
+++ Runner/Program.cs
@@ -1,15 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Diagnostics;
-using Implementations._2024._01;
+using Implementations._2024._02;
 
-var aoc =new Aoc202401();
+var aoc =new Aoc202402();
 var st  = new Stopwatch();
 st.Start();
 Console.WriteLine($"Part 1 Solution:{aoc.SolvePart1()}");
 st.Stop();
 Console.WriteLine($"Time for part 1:{st.ElapsedMilliseconds}ms");
+aoc =new Aoc202402();
 st.Restart();
-Console.WriteLine($"Part 1 Solution:{aoc.SolvePart2()}");
+Console.WriteLine($"Part 2 Solution:{aoc.SolvePart2()}");
 st.Stop();
 Console.WriteLine($"Time for part 2:{st.ElapsedMilliseconds}ms");
