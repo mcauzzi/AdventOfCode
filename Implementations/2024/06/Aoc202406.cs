69cc7f969028b4e951be5dfa1d448d14553e2ea4
mcauzzi <m.cauzzi93@gmail.com>
2024-12-10 00:52:14 +0100

    Update part 2 solution

--- Implementations/2024/06/Aoc202406.cs
+++ Implementations/2024/06/Aoc202406.cs
@@ -3,78 +3,86 @@ using Implementations.Helpers.Enums;
 
 namespace Implementations._2024._06;
 
+using Vector = (Coordinate Position, Coordinate Direction);
+
 public class Aoc202406 : IAoc<int, int>
 {
     public int SolvePart1()
     {
         LoadInput();
-        var guardPos         = GetGuardPosition();
-        var currDir          = PossibleMovements[Directions.UP];
+        var guardPos        = GetGuardPosition();
+        var currDir         = PossibleMovements[Directions.UP];
+        var patrolPositions = new List<Coordinate>();
         while (!guardPos.OutOfBounds(Input[0].Length, Input.Length))
         {
-            Input[guardPos.Y][guardPos.X] =  'X';
-            currDir                       =  GetNewDirection(guardPos, currDir);
-            guardPos                      += currDir;
+            patrolPositions.Add(guardPos);
+            currDir  =  GetNewDirection(guardPos, currDir);
+            guardPos += currDir;
         }
 
-        return Input.Sum(x => x.Count(y => y == 'X'));
+        return patrolPositions.Distinct().Count();
     }
 
     public int SolvePart2()
     {
         LoadInput();
-        var guardPos         = GetGuardPosition();
-        var currDir          = PossibleMovements[Directions.UP];
-        var guardStartingPos = guardPos;
+        var guardPos             = GetGuardPosition();
+        var currDir              = PossibleMovements[Directions.UP];
+        var patrolPositions      = new HashSet<Vector>();
+        var possibleObstructions = new List<Coordinate>();
         while (!guardPos.OutOfBounds(Input[0].Length, Input.Length))
         {
-            Input[guardPos.Y][guardPos.X] = PlaceGuardDirection(currDir);
-            var possibleLoopPos = guardPos + RotateDirection(currDir);
-            if (!possibleLoopPos.OutOfBounds(Input[0].Length, Input.Length)
-             && Input[possibleLoopPos.Y][possibleLoopPos.X] != LOOP)
-            {
-                if (currDir == PossibleMovements[Directions.UP]
-                 && Input[possibleLoopPos.Y][possibleLoopPos.X] == H_DIR)
-                {
-                    Input[possibleLoopPos.Y][possibleLoopPos.X] = LOOP;
-                }
-                else if (currDir == PossibleMovements[Directions.RIGHT]
-                      && Input[possibleLoopPos.Y][possibleLoopPos.X] == V_DIR)
-                {
-                    Input[possibleLoopPos.Y][possibleLoopPos.X] = LOOP;
-                }
-                else if (currDir == PossibleMovements[Directions.DOWN]
-                      && Input[possibleLoopPos.Y][possibleLoopPos.X] == H_DIR)
-                {
-                    Input[possibleLoopPos.Y][possibleLoopPos.X] = LOOP;
-                }
-                else if (currDir == PossibleMovements[Directions.LEFT]
-                      && Input[possibleLoopPos.Y][possibleLoopPos.X] == V_DIR)
-                {
-                    Input[possibleLoopPos.Y][possibleLoopPos.X] = LOOP;
-                }
-            }
-
+            patrolPositions.Add((guardPos, currDir));
+            CheckLoop(currDir, guardPos, patrolPositions,possibleObstructions);
             currDir  =  GetNewDirection(guardPos, currDir);
             guardPos += currDir;
         }
 
-        foreach (var c in Input)
+        return possibleObstructions.Count();
+    }
+
+    private void CheckLoop(Coordinate currDir, Coordinate guardPos, HashSet<Vector> patrolPositions,List<Coordinate> Obstructions)
+    {
+        var possibleObstruction = guardPos + currDir;
+        if (possibleObstruction.OutOfBounds(Input[0].Length, Input.Length) 
+         || Input[possibleObstruction.Y][possibleObstruction.X] == Obstacle
+            ||Obstructions.Contains(possibleObstruction))
         {
-            Console.WriteLine(new string(c));
+            return;
         }
-        return Input.Sum(x => x.Count(y => y == LOOP));
+        Input[possibleObstruction.Y][possibleObstruction.X] = Obstacle;
+        var loopDirection=GetNewDirection(guardPos,currDir);
+        var loopPosition  = guardPos + loopDirection;
+        var loopPositions = new HashSet<Vector>();
+        while (!loopPosition.OutOfBounds(Input[0].Length, Input.Length))
+        {
+            if (!loopPositions.Add((loopPosition, loopDirection)))
+            {
+                Obstructions.Add(possibleObstruction);
+                Input[possibleObstruction.Y][possibleObstruction.X] = '.';
+                return;
+            }
+            if(patrolPositions.Contains((loopPosition, loopDirection)))
+            {
+                Obstructions.Add(possibleObstruction);
+                Input[possibleObstruction.Y][possibleObstruction.X] = '.';
+                return;
+            }
+            loopDirection =  GetNewDirection(loopPosition, loopDirection);
+            loopPosition  += loopDirection;
+        }
+        Input[possibleObstruction.Y][possibleObstruction.X] = '.';
     }
 
     private void LoadInput()
     {
-        Input = File.ReadAllLines("2024/06/Test.txt").Select(x => x.ToCharArray()).ToArray();
+        Input = File.ReadAllLines("2024/06/Input.txt").Select(x => x.ToCharArray()).ToArray();
     }
 
-    private Position GetNewDirection(Position guardPos, Position currDir)
+    private Coordinate GetNewDirection(Coordinate guardPos, Coordinate currDir)
     {
         var newPos = guardPos + currDir;
-        while (!newPos.OutOfBounds(Input[0].Length, Input.Length) && Input[newPos.Y][newPos.X] == OBSTACLE)
+        while (!newPos.OutOfBounds(Input[0].Length, Input.Length) && Input[newPos.Y][newPos.X] == Obstacle)
         {
             currDir = RotateDirection(currDir);
             newPos  = guardPos + currDir;
@@ -83,7 +91,7 @@ public class Aoc202406 : IAoc<int, int>
         return currDir;
     }
 
-    private Position RotateDirection(Position currDir)
+    private Coordinate RotateDirection(Coordinate currDir)
     {
         return currDir switch
                {
@@ -95,25 +103,16 @@ public class Aoc202406 : IAoc<int, int>
                };
     }
 
-    private char PlaceGuardDirection(Position currDir) =>
-        currDir switch
-        {
-            { X: 1, Y : 0 }  => H_DIR,
-            { X: -1, Y: 0 }  => H_DIR,
-            { X: 0, Y : 1 }  => V_DIR,
-            { X: 0, Y : -1 } => V_DIR,
-        };
-
-    private Position GetGuardPosition()
+    private Coordinate GetGuardPosition()
     {
         for (int i = 0; i < Input.Length; i++)
         {
             var currLine = Input[i];
             for (int j = 0; j < currLine.Length; j++)
             {
-                if (currLine[j] == GUARD)
+                if (currLine[j] == Guard)
                 {
-                    return new Position(j, i);
+                    return new Coordinate(j, i);
                 }
             }
         }
@@ -122,23 +121,20 @@ public class Aoc202406 : IAoc<int, int>
     }
 
     private       char[][] Input { get; set; }
-    private const char     GUARD    = '^';
-    private const char     OBSTACLE = '#';
-    private const char     H_DIR    = '-';
-    private const char     V_DIR    = '|';
-    private const char     LOOP     = 'O';
+    private const char     Guard    = '^';
+    private const char     Obstacle = '#';
 
-    private Dictionary<Directions, Position> PossibleMovements = new()
-                                                                 {
-                                                                     { Directions.UP, new(0, -1) },
-                                                                     { Directions.DOWN, new(0, 1) },
-                                                                     { Directions.LEFT, new(-1, 0) },
-                                                                     { Directions.RIGHT, new(1, 0) }
-                                                                 };
+    private Dictionary<Directions, Coordinate> PossibleMovements = new()
+                                                                   {
+                                                                       { Directions.UP, new(0, -1) },
+                                                                       { Directions.DOWN, new(0, 1) },
+                                                                       { Directions.LEFT, new(-1, 0) },
+                                                                       { Directions.RIGHT, new(1, 0) }
+                                                                   };
 }
 
-public record Position(int X, int Y)
+public record Coordinate(int X, int Y)
 {
-    public static Position operator +(Position left, Position right) => new(left.X + right.X, left.Y + right.Y);
-    public        bool OutOfBounds(int         maxX, int      maxY)  => X < 0 || Y < 0 || X >= maxX || Y >= maxY;
+    public static Coordinate operator +(Coordinate left, Coordinate right) => new(left.X + right.X, left.Y + right.Y);
+    public        bool OutOfBounds(int             maxX, int        maxY)  => X < 0 || Y < 0 || X >= maxX || Y >= maxY;
 }
