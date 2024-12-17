417e933724ff6d4b4f271f7a6acaafd130e6f967
marco <furai93@gmail.com>
2024-12-17 21:33:48 +0100

    Added day 10

--- Implementations/2024/10/Aoc202410.cs
+++ Implementations/2024/10/Aoc202410.cs
@@ -2,15 +2,101 @@ using Implementations.Helpers;
 
 namespace Implementations._2024._10;
 
-public class Aoc202410:IAoc<long,long>
+public class Aoc202410 : IAoc<long, long>
 {
+    public Aoc202410()
+    {
+        Input = File.ReadAllLines("2024/10/Input.txt")
+                    .Select(x => x.Select(x => int.Parse(x.ToString())).ToArray())
+                    .ToArray();
+    }
+
+    public int[][] Input { get; set; }
+
     public long SolvePart1()
     {
-        throw new NotImplementedException();
+        var trailHeads = new List<Coordinate>();
+        for (int i = 0; i < Input.Length; i++)
+        {
+            var currLine = Input[i];
+            for (int j = 0; j < currLine.Length; j++)
+            {
+                if (currLine[j] == 0)
+                {
+                    trailHeads.Add(new Coordinate(j, i));
+                }
+            }
+        }
+
+        var trailsTails = new Dictionary<Coordinate, List<Coordinate>>();
+        for (var i = 0; i < trailHeads.Count; i++)
+        {
+            var head  = trailHeads[i];
+            var tails = new List<Coordinate>();
+            SearchTrails(head, tails);
+            trailsTails.Add(head,tails);
+        }
+
+        return trailsTails.SelectMany(x=>x.Value.Distinct()).Count();
+    }
+
+    private void SearchTrails(Coordinate head, List<Coordinate> trailTails)
+    {
+        if (head.OutOfBounds(Input[0].Length, Input.Length))
+        {
+            return;
+        }
+
+        var currValue = Input[head.Y][head.X];
+        if (currValue == 9)
+        {
+            trailTails.Add(head);
+            return;
+        }
+
+        foreach (var coordinate in Coordinate._4DirectionsVectors)
+        {
+            var nextStep = head + coordinate.Value;
+            if (nextStep.OutOfBounds(Input[0].Length, Input.Length))
+            {
+                continue;
+            }
+
+            var nextValue = Input[nextStep.Y][nextStep.X];
+
+            if ((nextValue - currValue) != 1)
+            {
+                continue;
+            }
+
+            SearchTrails(nextStep, trailTails);
+        }
     }
 
     public long SolvePart2()
     {
-        throw new NotImplementedException();
+        var trailHeads = new List<Coordinate>();
+        for (int i = 0; i < Input.Length; i++)
+        {
+            var currLine = Input[i];
+            for (int j = 0; j < currLine.Length; j++)
+            {
+                if (currLine[j] == 0)
+                {
+                    trailHeads.Add(new Coordinate(j, i));
+                }
+            }
+        }
+
+        var trailsTails = new Dictionary<Coordinate, List<Coordinate>>();
+        for (var i = 0; i < trailHeads.Count; i++)
+        {
+            var head  = trailHeads[i];
+            var tails = new List<Coordinate>();
+            SearchTrails(head, tails);
+            trailsTails.Add(head,tails);
+        }
+
+        return trailsTails.SelectMany(x=>x.Value).Count();
     }
 }
--- Implementations/Helpers/Coordinate.cs
+++ Implementations/Helpers/Coordinate.cs
@@ -1,3 +1,4 @@
+using System.Collections.Immutable;
 using Implementations.Helpers.Enums;
 
 namespace Implementations.Helpers;
@@ -8,13 +9,14 @@ public record Coordinate(int X, int Y)
     public static Coordinate operator -(Coordinate left, Coordinate right) => new(left.X - right.X, left.Y - right.Y);
     public        bool       OutOfBounds(int       maxX, int        maxY)  => X < 0 || Y < 0 || X >= maxX || Y >= maxY;
     public        Coordinate Abs()                              => new Coordinate(Math.Abs(X), Math.Abs(Y));
-    public static Coordinate GetDirectionVector(Directions dir) => DirectionsVectors[dir];
+    public static Coordinate GetDirectionVector(Directions dir) => _4DirectionsVectors[dir];
 
-    private static Dictionary<Directions, Coordinate> DirectionsVectors = new()
-                                                                          {
-                                                                              { Directions.UP, new(0, -1) },
-                                                                              { Directions.DOWN, new(0, 1) },
-                                                                              { Directions.LEFT, new(-1, 0) },
-                                                                              { Directions.RIGHT, new(1, 0) }
-                                                                          };
+    public static readonly ImmutableDictionary<Directions, Coordinate> _4DirectionsVectors =
+        (new Dictionary<Directions, Coordinate>()
+         {
+             { Directions.UP, new(0, -1) },
+             { Directions.DOWN, new(0, 1) },
+             { Directions.LEFT, new(-1, 0) },
+             { Directions.RIGHT, new(1, 0) }
+         }).ToImmutableDictionary();
 }
