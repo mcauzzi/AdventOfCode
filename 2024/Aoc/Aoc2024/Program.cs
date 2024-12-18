c76bf5f9c869d628286fc82d3dd2246d1993f2c0
mcauzzi <furai93@gmail.com>
2024-12-18 11:32:42 +0000

    edited the correct program.cs removed useless folder

--- 2024/Aoc/Aoc2024/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Aoc2024._01;
-
-var sol=new Solution0124();
-Console.WriteLine(sol.Part1());
-Console.WriteLine(sol.Part2());
